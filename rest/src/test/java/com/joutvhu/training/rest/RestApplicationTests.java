26915da4968e1677aa6e77fd4c94ee10fc69a03b
Giao Ho <joutvhu@gmail.com>
2021-05-01 19:08:49 +0700

    Init rest project

---
+++ rest/src/test/java/com/joutvhu/training/rest/RestApplicationTests.java
@@ -0,0 +1,11 @@
+package com.joutvhu.training.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
